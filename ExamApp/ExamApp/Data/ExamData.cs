f23842a4a7b58592dd3c4e7704da26c4ee1eb224
Renen <Renen@DESKTOP-1B94TU9>
2026-09-09 23:01:07 +0800

    Added in-memory ExamData to store question and answers

---
+++ ExamApp/ExamApp/Data/ExamData.cs
@@ -0,0 +1,12 @@
+﻿using ExamApp.Models;
+
+namespace ExamApp.Data
+{
+    public static class ExamData
+    {
+        public static List<QuestionAnswer> Questions = new List<QuestionAnswer>
+        {
+            
+        };
+    }
+}
