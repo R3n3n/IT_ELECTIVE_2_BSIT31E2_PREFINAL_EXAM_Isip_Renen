929bd37afd4e68ae0a8536b26c8633c4b897cb0a
Renen <Renen@DESKTOP-1B94TU9>
2026-09-09 22:57:59 +0800

    Added QuestionAnswer Model

---
+++ ExamApp/ExamApp/Models/QuestionAnswer.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamApp.Models
+{
+    public class QuestionAnswer
+    {
+        public int Number { get; set; }
+        public string Topic { get; set; } = string.Empty;
+        public string Question { get; set; } = string.Empty;
+        public Dictionary<string, string> Options { get; set; } = new Dictionary<string, string>();
+        public string CorrectLetter { get; set; } = string.Empty;
+    }
+}
