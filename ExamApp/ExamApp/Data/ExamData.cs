7255893db08b1d3c26577c420f8301c1d9e3be9a
Renen <Renen@DESKTOP-1B94TU9>
2026-09-09 23:13:35 +0800

    Answer item #2: Database-First

--- ExamApp/ExamApp/Data/ExamData.cs
+++ ExamApp/ExamApp/Data/ExamData.cs
@@ -13,6 +13,13 @@ namespace ExamApp.Data
                 Options = new Dictionary<string, string> { { "A", "It makes C# code shorter" }, { "B", "It prevents the application from restarting" }, { "C", "It allows data to persist after the application stops" }, { "D", "It removes the need for MVC" } },
                 CorrectLetter = "C"
             },
+            new QuestionAnswer {
+                Number = 2,
+                Topic = "EF Core Basics",
+                Question = "Which approach is being used when an existing database is used to generate EF Core entity classes?",
+                Options = new Dictionary<string, string> { { "A", "Code-First" }, { "B", "Database-First" }, { "C", "Model-First" }, { "D", "Controller-First" } },
+                CorrectLetter = "B"
+            },
         };
     }
 }
